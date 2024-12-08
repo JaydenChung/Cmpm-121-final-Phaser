b2b7f27ea9d17044f047159e39e8ab2cf2252a21
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-12-07 21:46:07 -0800

    ui change

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -9,7 +9,14 @@ class StartScene extends Phaser.Scene {
     }
 
     create() {
-        document.getElementById('description').innerHTML = '<h1>Graceful Garden<br>[O] Next Turn<br>[Z] Undo<br>[X] Redo<br>[F] Save Game<br>[L] Load Game'
+        document.getElementById('description').innerHTML = `
+        <h1>Graceful Garden
+        <pre>
+        [A] ⟵   [W] ↑   [S] ↓  [D] ⟶
+        [1] 1st Plant   [2] 2nd Plant   [3] 3rd Plant
+        [O] Next Turn
+        [Z] Undo        [X] Redo
+        [F] Save Game   [L] Load Game`;
         // Add background image
         const bg = this.add.image(0, 0, 'mapBG').setOrigin(0, 0);
         bg.displayWidth = this.sys.game.config.width;
@@ -22,26 +29,6 @@ class StartScene extends Phaser.Scene {
             50,
             "Graceful Garden",
             { fontSize: '48px', color: '#ffffff', fontFamily: 'Arial' }
-        ).setOrigin(0.5);
-
-        // Add controls info
-        const controlsText = `
-        Controls:
-        W - Move Up
-        S - Move Down
-        A - Move Left
-        D - Move Right
-        O - End Turn
-        Z - Undo
-        X - Redo
-        F - Save Game
-        L - Load Game`;
-        
-        this.add.text(
-            this.sys.game.config.width / 2,
-            200,
-            controlsText,
-            { fontSize: '20px', color: '#ffffff', fontFamily: 'Courier', align: 'center', lineSpacing: 10 }
         ).setOrigin(0.5);
 
         // Add start button
